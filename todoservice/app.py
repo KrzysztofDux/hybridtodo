5d9af3440c74069af7fd8a14979ebb657044e194
Krzysztof Kulak <krzysztofpiotrdux@gmail.com>
2019-09-12 21:25:49 +0200

    add db connection for flask app

--- todoservice/app.py
+++ todoservice/app.py
@@ -1,5 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db_todo/todo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        show_len = 20
+        return self.content if len(self.content) < show_len else f'{self.content}...'
+
 
 @app.route('/')
 def hello_world():
